4dbb41d0f54280b61a3d10f84bd7d88026d6c32d
Paolo Combes <paolocombes@gmail.com>
2023-07-21 11:11:34 +0200

    updated config and added data/tal_noisemaker/render_parameter_variations.py

--- src/data/tal_noisemaker/__init__.py
+++ src/data/tal_noisemaker/__init__.py
@@ -1,1 +1,16 @@
 from .noisemaker_dataset import NoisemakerVariationsDataset
+from . import parameter_variations
+
+# from .parameter_variations import (
+#     amp_attack,
+#     amp_decay,
+#     filter_cutoff,
+#     filter_decay,
+#     filter_resonance,
+#     frequency_mod,
+#     lfo_amount_on_filter,
+#     lfo_amount_on_volume,
+#     lfo_rate_on_filter,
+#     lfo_rate_on_volume,
+#     reverb,
+# )
